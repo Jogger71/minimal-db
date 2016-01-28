ece53a7e49cdfa04b63740a9db4a5fc9faef0778
Adrian Du Plessis <adrian_dp@outlook.com>
2016-01-28 23:31:40 +0200

    Added version 1.0.0 of the query class.

---
+++ minimal-db/query.php
@@ -0,0 +1,186 @@
+<?php
+
+
+/**
+ * Sql Query
+ *
+ * @version 1.0.0
+ * @since 1.0.0
+ */
+
+class Query {
+	/**
+	 * The final query string
+	 *
+	 * @var string $query
+	 * @since 1.0.0
+	 */
+	private $query;
+
+	/**
+	 * Class constructor
+	 *
+	 * @param string $type
+	 * @param string $table
+	 * @param array $data
+	 * @param array $where
+	 * @since 1.0.0
+	 */
+	public function __construct( $type, $table, $data, $where = array() ) {
+		switch ( $type ) {
+			case 'select':
+				$this->query = $this->generateSelectQuery( $table, $data, $where );
+				break;
+			case 'insert':
+				$this->query = $this->generateInsertQuery( $data, $table );
+				break;
+			case 'update':
+				$this->query = $this->generateUpdateQuery( $data, $table, $where );
+				break;
+			case 'delete':
+				$this->query = $this->generateDeleteQuery( $table, $data );
+				break;
+		}
+	}
+
+	/**
+	 * Returns the query
+	 *
+	 * @return string
+	 * @since 1.0.0
+	 */
+	public function getQuery() {
+		return $this->query;
+	}
+
+	/**
+	 * Generates a SELECT query
+	 *
+	 * @param array|string $columns
+	 * @param string $table
+	 * @param array $where
+	 * @return string
+	 * @since 1.0.0
+	 */
+	public function generateSelectQuery( $table, $columns = '*', $where = array() ) {
+		if ( is_array( $columns ) ) {
+			$columnString = implode( ', ', $columns );
+		} else  {
+			$columnString = $columns;
+		}
+
+		$selectQuery = "SELECT $columnString FROM $table";
+
+		if ( ! empty( $where ) ) {
+			$whereString = $this->generateWhere( $where );
+			$selectQuery .= " WHERE $whereString";
+		}
+
+		$selectQuery .= ";";
+
+		return $selectQuery;
+	}
+
+	/**
+	 * Generates a INSERT query
+	 *
+	 * @param array $data
+	 * @param string $table
+	 * @return string
+	 * @since 1.0.0
+	 */
+	public function generateInsertQuery( $data, $table ) {
+		$columns = array();
+		$values = array();
+
+		foreach( $data as $k => $v ) {
+			$columns[] = $k;
+			if ( is_string( $v ) ) {
+				$values[] = '"' . $v . '"';
+			} else {
+				$values[] = $v;
+			}
+		}
+
+		$columnsString = implode( ', ', $columns );
+		$valuesString = implode( ', ', $values );
+
+		$insertQuery = "INSERT INTO $table ( $columnsString ) VALUES ( $valuesString );";
+
+		return $insertQuery;
+	}
+
+	/**
+	 * Generates an UPDATE query
+	 *
+	 * @param array $data
+	 * @param string $table
+	 * @param array $where
+	 * @return string
+	 * @since 1.0.0
+	 */
+	public function generateUpdateQuery( $data, $table, $where = array() ) {
+		$setValues = array();
+
+		foreach( $data as $k => $v ) {
+			$setValues[] = ( is_string($v) ) ? sprintf( '%1$s="%2$s"', $k, $v ) : sprintf( '%1$s=%2$s', $k, $v );
+		}
+
+		$setValuesString = implode( ', ', $setValues );
+
+		$updateQuery = sprintf( 'UPDATE %1$s SET %2$s', $table, $setValuesString );
+
+		if ( ! empty( $where ) ) {
+			$whereString = $this->generateWhere( $where );
+			$updateQuery .= " WHERE $whereString";
+		}
+
+		$updateQuery .= ";";
+
+		return $updateQuery;
+	}
+
+	/**
+	 * Generates a DELETE query
+	 *
+	 * @param string $table
+	 * @param array $where
+	 * @return string
+	 * @since 1.0.0
+	 */
+	public function generateDeleteQuery( $table, $where ) {
+		$deleteQuery = sprintf( 'DELETE FROM %s', $table );
+
+		if ( ! empty( $where ) ) {
+			$whereString = $this->generateWhere( $where );
+			$deleteQuery .= " WHERE $whereString";
+		}
+
+		$deleteQuery .= ';';
+
+		return $deleteQuery;
+	}
+
+	/**
+	 * Generates the WHERE section
+	 *
+	 * @param array $where
+	 * @return string
+	 * @since 1.0.0
+	 */
+	public function generateWhere( $where ) {
+		$whereStrings = array();
+
+		foreach ( $where as $key => $value ) {
+			if ( is_string( $value ) ) {
+				$whereStrings[] = sprintf( '%1$s="%2$s"', $key, $value );
+			} else {
+				$whereStrings[] = sprintf( '%1$s=%2$s', $key, $value );
+			}
+		}
+
+		$whereQuery = implode( ' AND ', $whereStrings );
+
+		return $whereQuery;
+	}
+}
